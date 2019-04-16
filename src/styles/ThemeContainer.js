f0c4996a10052c3ce36762142785d2cc9e7f3527
Jonathan Cunanan <j_cunanan05@yahoo.com>
2019-04-16 18:09:47 -0400

    Add Theme Provider

---
+++ src/styles/ThemeContainer.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+
+const theme = createMuiTheme();
+
+function ThemeContainer(WrappedComponent) {
+  class EnhancedComponent extends Component {
+    render() {
+      return (
+        <MuiThemeProvider theme={theme}>
+          <WrappedComponent />
+        </MuiThemeProvider>
+      );
+    }
+  }
+
+  EnhancedComponent.displayName = `ThemeContainer(${WrappedComponent.displayName ||
+    WrappedComponent.name ||
+    'Component'})`;
+
+  return EnhancedComponent;
+}
+
+export default ThemeContainer;
